046788d408975fe6d31d14f148cc1bb09a1baffa
Yandere Kain <Logoskun@gmail.com>
2025-07-11 15:07:19 +0300

    AbstractFactory +Enum

---
+++ AbstractFactory/AbstractFactoryClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.AbstractFactory
+{
+    public static class AbstractFactoryClass
+    {
+        public static void AbstractFactory()
+        {
+            Client client = new Client();
+
+            IAbstractFactory FactoryA = client.CreateFactory(FactoryType.A);
+            Console.WriteLine(FactoryA.CreateProductA().Name);
+            Console.WriteLine(FactoryA.CreateProductB().Name);
+
+            IAbstractFactory FactoryB = client.CreateFactory(FactoryType.B);
+            Console.WriteLine(FactoryB.CreateProductA().Name);
+            Console.WriteLine(FactoryB.CreateProductB().Name);
+
+            try
+            {
+                IAbstractFactory FactoryError = client.CreateFactory(FactoryType.None);
+                Console.WriteLine(FactoryError.CreateProductA().Name);
+                Console.WriteLine(FactoryError.CreateProductB().Name);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,35 +1,11 @@
-﻿using Patterns.AbstractFactory;
-
+﻿
 class Program
 {
     static async Task Main(string[] args)
     {
-        //AbstractFactory();
+        //Patterns.AbstractFactory.AbstractFactoryClass.AbstractFactory();
 
         Console.ReadLine();
-    }
-    static void AbstractFactory()
-    {
-        Client client = new Client();
-
-        IAbstractFactory FactoryA = client.CreateFactory(FactoryType.A);
-        Console.WriteLine(FactoryA.CreateProductA().Name);
-        Console.WriteLine(FactoryA.CreateProductB().Name);
-
-        IAbstractFactory FactoryB = client.CreateFactory(FactoryType.B);
-        Console.WriteLine(FactoryB.CreateProductA().Name);
-        Console.WriteLine(FactoryB.CreateProductB().Name);
-
-        try
-        {
-            IAbstractFactory FactoryError = client.CreateFactory(FactoryType.None);
-            Console.WriteLine(FactoryError.CreateProductA().Name);
-            Console.WriteLine(FactoryError.CreateProductB().Name);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-        }
     }
 
 }
