2d52c9efbb1636411ac8c1b986c398a5e32eb106
Yandere Kain <Logoskun@gmail.com>
2025-07-14 15:15:12 +0300

    Singleton

--- Singleton/SingletonRegistry.cs
+++ Singleton/SingletonRegistry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace Patterns.Singleton
 {
     internal class SingletonRegistry
     {
-        private static readonly List<ISingleton> _instances = new();
+        private static readonly ConcurrentBag<ISingleton> _instances = new();
 
         public static void Register(ISingleton instance)
         {
